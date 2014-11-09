b9a9ed530974f9a9d8edc37c1c6664f88a7944e1
Sebastian Garzon <js.garzon398@uniandes.edu.co>
2014-11-08 20:38:51 -0500

    Final comentado

--- CuerdaVibrando/grafica.py
+++ CuerdaVibrando/grafica.py
@@ -2,14 +2,17 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+#metodo que toma el nombre del archivo y carga la informacion
 nombreArchivo = sys.argv[1]
 data = np.loadtxt(nombreArchivo)
 name=nombreArchivo.replace(".dat","")
 y = data;
+#metodo que crea los arrays para poder plotear en 3d
 x = np.zeros(shape=(121,101))
 t= np.zeros(shape=(121,101))
 contador=0
 contador1=0
+#metodo que rellena los arrays de la forma deseada
 for i in range (0,101):
     for j in range(0,121):
         x[j][i]=contador
@@ -18,6 +21,7 @@ for i in range (0,121):
     for j in range(0,101):
         t[i][j]=contador1
     contador1+=1
+#metodo que plotea la superficie generada.
 fig = plt.figure(figsize=(9, 6))
 ax = fig.add_subplot(111, projection = '3d')
 ax.plot_wireframe(t,x,y,rstride=2,cstride=1,  color ='y')
@@ -26,4 +30,5 @@ ax.set_xlabel('tiempo',fontsize=15)
 ax.set_ylabel('Eje x',fontsize=15)
 ax.set_zlabel('Eje Y',fontsize=15)
 ax.set_title('Comportamiento cuerda',fontsize=20)
+#metodo que guarda la imagen
 plt.savefig(name+'.pdf', format = 'pdf', transparent = True)
